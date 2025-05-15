30357761d47a696108ccaad4d3240bdd9fa20e04
Jiahao Yuan <jiahaoyuan55@gmail.com>
2025-05-15 11:27:49 -0400

    Initial commit with backend and frontend folders

---
+++ frontend/Banky/Banky/BankyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankyApp.swift
+//  Banky
+//
+//  Created by Jiahao Yuan on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct BankyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frontend/Banky/BankyTests/BankyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BankyTests.swift
+//  BankyTests
+//
+//  Created by Jiahao Yuan on 5/15/25.
+//
+
+import Testing
+@testable import Banky
+
+struct BankyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
